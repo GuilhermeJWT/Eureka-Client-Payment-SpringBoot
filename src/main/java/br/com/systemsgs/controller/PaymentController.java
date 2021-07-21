399287861aaba13130a988acb9f5065252e22938
Guilherme Santos <guiromanno@gmail.com>
2021-07-21 18:23:31 -0300

    Primeiras Configurações do Controller

---
+++ src/main/java/br/com/systemsgs/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package br.com.systemsgs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/payment")
+public class PaymentController {
+	
+	@GetMapping(value = "/{preco}")
+	private String testePreco(Double preco) {
+		return "Preço: " + preco;
+	}
+
+}
